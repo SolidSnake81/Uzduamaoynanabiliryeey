30b1501216a42d999b35ba3722940b6c94a21843
SolidSnake81 <nakedsnake4life@gmail.com>
2020-10-29 16:55:06 +0300

    Boss_NOTFinished
     a

---
+++ Assets/Scripts/Grass.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grass : MonoBehaviour
+{
+
+    private Animator anim; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        anim = GetComponent<Animator>(); 
+
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if (other.CompareTag("Player"))
+        {
+            anim.SetTrigger("Trigger");
+        } 
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class MainMenu : MonoBehaviour
+{
+
+    public GameObject panel;  
+    public void LoadScene (string sceneName)
+
+    {
+
+        StartCoroutine(Load(sceneName)); 
+    
+    }
+    
+    public void Quit()
+    {
+        Application.Quit(); 
+    }
+
+
+    IEnumerator    Load(string sceneName)
+
+    {
+        panel.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene(sceneName);
+    }
+    // Start is called before the first frame update
+    void Start()
+    { 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
